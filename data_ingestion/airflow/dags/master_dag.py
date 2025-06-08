bb03c91a8b963967c4a040df8dcb910b6cf2ca57
CeciPePe <ceci.perez.perez@gmail.com>
2025-06-08 20:50:07 +0200

    fix missng dags

--- data_ingestion/airflow/dags/master_dag.py
+++ data_ingestion/airflow/dags/master_dag.py
@@ -2,7 +2,7 @@ from airflow import DAG
 from airflow.operators.trigger_dagrun import TriggerDagRunOperator
 from datetime import datetime
 
-dag_ids = ["ingest_emissions_data_dag", "ingest_environmental_indicators_dag", "ingest_gencat_incidents_dag", "ingest_renfe_incidents_dag", "ingest_google_maps_images_dag" , "ingest_passenger_volume_dag", "ingest_serapi_images_dag","ingest_twitter_data_dag","ingest_admin_shapefiles_dag" , "emissions_to_influxdb"]       
+dag_ids = ["ingest_emissions_data_dag", "ingest_environmental_indicators_dag", "ingest_gencat_incidents_dag", "ingest_renfe_incidents_dag", "ingest_google_maps_images_dag" , "ingest_passenger_volume_dag", "ingest_serapi_images_dag","ingest_twitter_data_dag","ingest_admin_shapefiles_dag" , "ingest_emissions_to_influxdb_dag", "ingest_trafffic_to_influxdb_dag"]       
 
 
 with DAG("master_dag", start_date=datetime(2024, 1, 1), schedule=None, catchup=False) as dag:
